c2894992127c6a00c1757f2c3475450f72c3847b
radolf-edge <radolf@bluebash.co>
2022-05-24 15:52:54 +0530

    Added sidebar for report analysis

---
+++ app/overrides/spree/admin/shared/add_analysis_tab_to_main_menu.rb
@@ -0,0 +1,12 @@
+Deface::Override.new(
+  virtual_path: 'spree/admin/shared/_main_menu',
+  name: 'add_analysis_tab_to_admin_main_menu',
+  insert_before: "erb[silent]:contains('if can? :admin, current_store')",
+  text: <<-HTML
+    <% if can? :admin, current_store %>
+      <ul class="nav nav-sidebar border-bottom" id="sidebarAnalysis">
+        <%= main_menu_tree Spree.t(:tab_heading, scope: :analysis), icon: "report.svg", sub_menu: "analysis_reports", url: "#sidebar-analysis" %>
+      </ul>
+    <% end %>
+  HTML
+)
