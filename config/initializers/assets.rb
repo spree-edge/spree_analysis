60ab95cfca7e27d36cdaf17cb315fe47e48bf982
radolf-edge <radolf@bluebash.co>
2022-05-30 10:51:56 +0530

    Added assets and external JS

---
+++ config/initializers/assets.rb
@@ -0,0 +1,1 @@
+Rails.application.config.assets.precompile += %w( spree/backend/spree_analysis.js )
--- lib/generators/spree_analysis/install/install_generator.rb
+++ lib/generators/spree_analysis/install/install_generator.rb
@@ -8,6 +8,14 @@ module SpreeAnalysis
         copy_file 'spree_analysis.rb', 'config/initializers/spree_analysis.rb'
       end
 
+      def add_javascripts
+        append_file 'vendor/assets/javascripts/spree/backend/all.js', "//= require spree/backend/spree_analysis\n"
+      end
+
+      def add_stylesheets
+        inject_into_file 'vendor/assets/stylesheets/spree/backend/all.css', " *= require spree/backend/spree_analysis\n", :before => /\*\//, :verbose => true
+      end
+
       def add_migrations
         run 'bundle exec rake railties:install:migrations FROM=spree_analysis'
       end
