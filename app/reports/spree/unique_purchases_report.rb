5c40e6f5d65e133f09cdb481a97bb913480efa39
saga-edge <saga@bluebash.co>
2022-08-19 16:00:16 +0530

    Merge branch 'enhance/user-analysis' into combine/all-1

---
+++ app/reports/spree/unique_purchases_report.rb
@@ -0,0 +1,39 @@
+module Spree
+  class UniquePurchasesReport < Spree::Report
+    DEFAULT_SORTABLE_ATTRIBUTE = :product_name
+    HEADERS                    = { sku: :string, product_name: :string, sold_count: :integer, users: :integer }
+    SEARCH_ATTRIBUTES          = { start_date: :orders_completed_from, end_date: :orders_completed_till }
+    SORTABLE_ATTRIBUTES        = [:product_name, :sku, :sold_count, :users]
+
+    deeplink product_name: { template: %Q{<a href="/admin/products/{%# o.product_slug %}" target="_blank">{%# o.product_name %}</a>} }
+
+    class Result < Spree::Report::Result
+      class Observation < Spree::Report::Observation
+        observation_fields [:product_name, :product_slug, :sku, :sold_count, :users]
+
+        def sku
+          @sku.presence || @product_name
+        end
+      end
+    end
+
+    def report_query
+      user_count_sql = '(COUNT(DISTINCT(spree_orders.email)))'
+      purchases_by_variant =
+        Spree::LineItem
+          .joins(:order)
+          .joins(:variant)
+          .joins(:product)
+          .where(spree_orders: { state: 'complete', completed_at: reporting_period })
+          .group('variant_id', 'spree_variants.sku', 'spree_products.slug', 'spree_products.name')
+          .select(
+            'spree_variants.sku   as sku',
+            'spree_products.slug  as product_slug',
+            'spree_products.name  as product_name',
+            'SUM(quantity)        as sold_count',
+            "#{ user_count_sql }  as users"
+          )
+    end
+
+  end
+end
---
+++ app/reports/spree/users_who_recently_purchased_report.rb
@@ -0,0 +1,70 @@
+module Spree
+  class UsersWhoRecentlyPurchasedReport < Spree::Report
+    DEFAULT_SORTABLE_ATTRIBUTE = :user_email
+    HEADERS                    = { user_email: :string, purchase_count: :integer, last_purchase_date: :date, last_purchased_order_number: :string }
+    SEARCH_ATTRIBUTES          = { start_date: :start_date, end_date: :end_date, email_cont: :email }
+    SORTABLE_ATTRIBUTES        = [:user_email, :purchase_count, :last_purchase_date]
+
+    def paginated?
+      true
+    end
+
+    class Result < Spree::Report::Result
+      class Observation < Spree::Report::Observation
+        observation_fields [:user_email, :last_purchased_order_number, :last_purchase_date, :purchase_count]
+
+        def last_purchase_date
+          @last_purchase_date.to_date.strftime("%B %d, %Y")
+        end
+      end
+    end
+
+    def record_count_query
+      Spree::Report::QueryFragments.from_subquery(report_query).project(Arel.star.count)
+    end
+
+    def report_query
+      ar_orders = Arel::Table.new(:spree_orders)
+      results = Arel::Table.new(:results)
+      Spree::Report::QueryFragments
+        .from_subquery(all_orders_with_users)
+        .join(ar_orders)
+        .on(
+          ar_orders[:email].eq(results[:user_email]).and(
+            ar_orders[:completed_at].eq(results[:last_purchase_date])
+          )
+        )
+        .project(
+          "results.user_email         as user_email",
+          "spree_orders.number        as last_purchased_order_number",
+          "results.last_purchase_date as last_purchase_date",
+          "results.purchased_count    as purchase_count"
+        )
+    end
+
+
+    def paginated_report_query
+      report_query
+        .take(records_per_page)
+        .skip(current_page)
+    end
+
+    private def all_orders_with_users
+      Spree::Order
+        .where(Spree::Order.arel_table[:email].matches(email_search))
+        .where(spree_orders: { completed_at: reporting_period })
+        .select(
+          "spree_orders.email             as user_email",
+          "max(spree_orders.completed_at) as last_purchase_date",
+          "count(spree_orders.email)      as purchased_count"
+        )
+        .group(
+          "user_email"
+        )
+    end
+
+    private def email_search
+      search[:email_cont].present? ? "%#{ search[:email_cont] }%" : '%'
+    end
+  end
+end
