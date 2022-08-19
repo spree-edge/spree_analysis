8b3308cafdee54e1a91005f1ed3acf0f29c065c2
saga-edge <saga@bluebash.co>
2022-08-19 15:29:29 +0530

    minor fix.

--- lib/generators/spree_analysis/install/spree_analysis.rb
+++ lib/generators/spree_analysis/install/spree_analysis.rb
@@ -2,7 +2,8 @@ reports = {
   finance_analysis:           [
     :payment_method_transactions, :payment_method_transactions_conversion_rate,
     :shipping_cost, :sales_tax, :sales_performance
-  ]
+  ],
+  promotion_analysis:         [:promotional_cost]
 }
 
 SpreeAnalysis::ReportConfig.configure do |config|
